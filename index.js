90bdce03ca5bb5c6ed90bec47ab545a2951cb50e
wamdidaniel <wamdidanielcollind256@gmail.com>
2023-02-14 21:55:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Javascrip Function
+
+function showMyName(){
+    console.log('My name is Daniel');
+
+}
+
+showMyName();
+showMyName('Kampi');
+
+function calcuSum(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+const result = calcuSum(30,20);
+console.log(result);
